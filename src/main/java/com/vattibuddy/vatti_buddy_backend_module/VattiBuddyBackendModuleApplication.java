f2477b28eb89e0aff05a080e845f641a7222d99e
Arnesh Rajendran <arnesha4261@gmail.com>
2025-10-05 14:38:24 +0530

    First intital commit

---
+++ src/main/java/com/vattibuddy/vatti_buddy_backend_module/VattiBuddyBackendModuleApplication.java
@@ -0,0 +1,13 @@
+package com.vattibuddy.vatti_buddy_backend_module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VattiBuddyBackendModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VattiBuddyBackendModuleApplication.class, args);
+	}
+
+}
